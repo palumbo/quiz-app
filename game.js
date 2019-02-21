32a865f43b89e9da081ce6ae2b017edfcf3c95db
Joseph Palumbo <p@lumbo.me>
2019-02-21 14:30:37 -0600

    adds game.js

---
+++ game.js
@@ -0,0 +1,85 @@
+const question = document.getElementById("question"); 
+const choices = Array.from(document.getElementsByClassName("choice-text")); 
+
+let currentQuestion = {}; 
+let acceptingAnswers = true; 
+let score = 0; 
+let questionCounter = 0; 
+let availableQuestions = []; 
+
+let questions = [
+    {
+        question: "Inside which HTML element do we put the JavaScript?", 
+        choice1: "<script>", 
+        choice2: "<javascript", 
+        choice3: "<js>", 
+        choice4: "<scripting>", 
+        answer: 1
+    }, 
+    {
+        question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
+        choice1: "<script href='xxx.js'>", 
+        choice2: "<script name='xxx.js'>",
+        choice3: "<script src='xxx.js'>",
+        choice4: "<script file='xxx.js'>",
+        answer: 3
+    }, 
+    {
+        question: "How do you write 'Hello World' in an alert box?",
+        choice1: "msgBox('Hello World');",
+        choice2: "alertBox('Hello World')", 
+        choice3: "msg('Hello World')",
+        choice4: "alert('Hello World')", 
+        answer:     4
+    }
+]
+
+// CONSTANTS
+const CORRECT_BONUS = 10; 
+const MAX_QUESTIONS = 3; 
+
+startGame = () => {
+    questionCounter = 0; 
+    score = 0; 
+    availableQuestions = [ ...questions]; 
+    getNewQuestion(); 
+}; 
+
+getNewQuestion = () => {
+
+    if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        // go to end page 
+        return window.location.assign("/end.html"); 
+    }
+
+    questionCounter++; // increments question counter
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length); 
+    currentQuestion = availableQuestions[questionIndex]; 
+    question.innerText = currentQuestion.question; 
+
+    choices.forEach(choice => {
+        const number = choice.dataset["number"]; 
+        choice.innerText = currentQuestion["choice" + number]; 
+    }); 
+
+    // removes question once asked
+    availableQuestions.splice(questionIndex, 1); 
+
+    acceptingAnswers = true; 
+}; 
+
+choices.forEach(choice => {
+    choice.addEventListener("click", e => {
+        if (!acceptingAnswers) return; 
+
+        acceptingAnswers = false; 
+
+        const selectedChoice = e.target; 
+        const selectedAnswer = selectedChoice.dataset['number']; 
+        console.log(selectedAnswer); 
+
+        getNewQuestion(); 
+    })
+})
+
+startGame(); 
